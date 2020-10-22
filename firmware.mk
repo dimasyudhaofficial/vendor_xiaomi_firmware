7075b56ee8f99a06cc4a5e83366f4d71a4386967
Dimas Yudha Pratama <dimasyudha@domainm.my.id>
2020-10-22 12:50:20 +0700

    Import Sirius firmware based on MIUI 12 stable V12.0.2.0

---
+++ firmware.mk
@@ -0,0 +1,20 @@
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/firmware/firmware-update/abl.elf:install/firmware-update/abl.elf \
+    vendor/xiaomi/firmware/firmware-update/aop.mbn:install/firmware-update/aop.mbn \
+    vendor/xiaomi/firmware/firmware-update/BTFM.bin:install/firmware-update/BTFM.bin \
+    vendor/xiaomi/firmware/firmware-update/cmnlib.mbn:install/firmware-update/cmnlib.mbn \
+    vendor/xiaomi/firmware/firmware-update/cmnlib64.mbn:install/firmware-update/cmnlib64.mbn \
+    vendor/xiaomi/firmware/firmware-update/devcfg.mbn:install/firmware-update/devcfg.mbn \
+    vendor/xiaomi/firmware/firmware-update/dspso.bin:install/firmware-update/dspso.bin \
+    vendor/xiaomi/firmware/firmware-update/dtbo.img:install/firmware-update/dtbo.img \
+    vendor/xiaomi/firmware/firmware-update/hyp.mbn:install/firmware-update/hyp.mbn \
+    vendor/xiaomi/firmware/firmware-update/keymaster64.mbn:install/firmware-update/keymaster64.mbn \
+    vendor/xiaomi/firmware/firmware-update/logfs_ufs_8mb.bin:install/firmware-update/logfs_ufs_8mb.bin \
+    vendor/xiaomi/firmware/firmware-update/logo.img:install/firmware-update/logo.img \
+    vendor/xiaomi/firmware/firmware-update/NON-HLOS.bin:install/firmware-update/NON-HLOS.bin \
+    vendor/xiaomi/firmware/firmware-update/qupv3fw.elf:install/firmware-update/qupv3fw.elf \
+    vendor/xiaomi/firmware/firmware-update/storsec.mbn:install/firmware-update/storsec.mbn \
+    vendor/xiaomi/firmware/firmware-update/tz.mbn:install/firmware-update/tz.mbn \
+    vendor/xiaomi/firmware/firmware-update/vbmeta.img:install/firmware-update/vbmeta.img \
+    vendor/xiaomi/firmware/firmware-update/xbl.elf:install/firmware-update/xbl.elf \
+    vendor/xiaomi/firmware/firmware-update/xbl_config.elf:install/firmware-update/xbl_config.elf
